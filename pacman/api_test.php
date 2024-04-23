e65983cc6782d918d08a8ddac4ac47a819ca2e44
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-04-23 21:10:34 +0100

    neater fix

--- pacman/api_test.php
+++ pacman/api_test.php
@@ -40,7 +40,7 @@ if($debug == 1) {echo " 2 ";}
     $level_pill_power = [0,90,5,3,2,1];
     $level_ghost_movement = [0,90,5,3,2,1];
     $fruit_values = [0,40,50,70,100];
-    $fruit_names = ["Cherry","Orange","Apple","Banana"];
+    $fruit_names = ["","Cherry","Orange","Apple","Banana"];
     $fruit_availability = [
     [],
     [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75],
@@ -229,7 +229,7 @@ while($e < count($teams_used)){
         if(in_array($cp,$cp_pills)){
             $powerup = $t + $level_pill_power[$current_level]*60;
             $powerup_time = floor($powerup/60)."m ".($powerup % 60)."s";
-            $results_detailed[$id][] = [$t,$cp,"Pill eaten - powered up until $t",0,$running_score];
+            $results_detailed[$id][] = [$t,$cp,"Pill eaten - powered up until $powerup_time",0,$running_score];
         }
 
         //eat ghost-point
