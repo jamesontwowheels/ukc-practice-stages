9230d03b66bf318f8f4a4612525b81f138cd82f8
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-07-16 20:49:01 +0000

    error checking

--- geo/assets/php/script.php
+++ geo/assets/php/script.php
@@ -9,8 +9,10 @@ echo "here";
 $sql = "INSERT INTO dbo.test_game (Player_ID, CP_ID, Time_stamp) VALUES 
     (1, 2, 3);";
 echo $sql;
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) == TRUE) {
     echo "record inserted successfully";
+    echo "The success inserted ID is: " . $last_id . "<br>";
 } else {
-    echo "record insert failed";
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "The last inserted ID is: " . $last_id . "<br>";
 }
