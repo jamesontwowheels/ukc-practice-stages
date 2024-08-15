92afcf8059ea2ca32c7f35023587386a989487c4
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-08-15 05:20:23 +0000

    unbroken echo

--- geo/assets/php/script.php
+++ geo/assets/php/script.php
@@ -1,20 +1,20 @@
 <?PHP
-
+$db_response = [];
 //generic CP hit
 $cp = $_REQUEST["cp"];
-echo "You hit CP $cp";
+$db_response[] = "You hit CP $cp";
 include 'db_connect.php';
 
 $sql = "INSERT INTO dbo.test_game (Player_ID, CP_ID, Time_stamp) VALUES 
     (1, $cp, 3);";
-echo $sql;
+$db_response[] = $sql;
 if ($conn->query($sql) == TRUE) {
-    echo "record inserted successfully";
+    $db_response[] =  "record inserted successfully";
     $last_id = $conn->insert_id;
-    echo "The success inserted ID is: " . $last_id . "<br>";
+    $db_response[] = "The success inserted ID is: " . $last_id . "<br>";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    echo "The last inserted ID is: " . $last_id . "<br>";
+    $db_response[] = "Error: " . $sql . "<br>" . $conn->error;
+    $db_response[] = "The last inserted ID is: " . $last_id . "<br>";
 }
 
 //include game rules + return something to the player
