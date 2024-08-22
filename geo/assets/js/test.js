a6005963e46ae6ab6e0138e21129b91714f00e2d
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-08-22 08:05:23 +0000

    empty the DOM

--- geo/assets/js/test.js
+++ geo/assets/js/test.js
@@ -28,6 +28,7 @@ function ajax_call() {
             const items = data["commentary"];
             items.reverse();
             const itemList = document.getElementById('commentary-list');
+            itemList.empty();
 
             // Loop through the array and append each item to the DOM
             items.forEach(item => {
