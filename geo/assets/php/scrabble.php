30893a400d93fe3ea9373ef6cbe23cd8c1721b6d
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-08-30 20:48:29 +0000

    list the players

--- geo/assets/php/scrabble.php
+++ geo/assets/php/scrabble.php
@@ -18,7 +18,12 @@ $i = 0;
 
 //build punches list
 $player_cps = [];
+$players = [];
 foreach ($result as $row) {
+    if(!in_array($row["Player_ID"],$players)){
+        $players[] = $row["Player_ID"];
+        $player_cps[] = $row["Player_ID"];
+    }
    $player_cps[$row["Player_ID"]][] = [$row["CP_ID"],$row["Time_stamp"]];
    $i += 1;
 }
