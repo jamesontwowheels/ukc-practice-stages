c8c508c84677439208ed1b0faaa8dd13b1b6fa8e
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-09-02 21:29:14 +0000

    variables

--- geo/assets/js/test.js
+++ geo/assets/js/test.js
@@ -1,4 +1,4 @@
-let countdownFunction;
+let countdownFunction = 0;
 console.log("cf defined");
 
 $(document).ready(ajax_call);
@@ -84,7 +84,7 @@ function ajax_call() {
             var game_start = parseInt(game_state[1]);
             var game_end = parseInt(game_state[2]);
             var stage_time = game_state[3];
-            if(game_state[0] === 1 && typeof countdownFunction === "undefined"){
+            if(game_state[0] === 1 && typeof countdownFunction > 0){
                     // Set the date and time we're counting down to
                 const countdownDate = game_start + stage_time; // 5 minutes from now
                 // Update the countdown every second
