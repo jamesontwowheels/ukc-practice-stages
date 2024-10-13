2275eaeb3c2f916b2a763f120c22181973826371
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-10-13 20:28:30 +0000

    main.css

--- stages.php
+++ stages.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['username'])) {
 }?>
 <head>
 
-<link rel="stylesheet" href="geo/assets/css/main.css">
+<link rel="stylesheet" href="assets/css/main.css">
 <link rel="manifest" href="/manifest.json">
 
 </head>
