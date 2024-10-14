025f7def6de51918a252f10d67e4112449629825
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-10-14 21:08:51 +0000

    simples?

--- deep_blue/assets/js/test.js
+++ deep_blue/assets/js/test.js
@@ -240,8 +240,8 @@ function ajax_call() {
                     const distance = countdownDate - now_s;
 
                     // Time calculations for minutes and seconds
-                    const minutes = Math.floor((distance) / (60));
-                    const seconds = Math.floor(distance % (60));
+                    var minutes = Math.floor((distance) / (60));
+                    var seconds = Math.floor(distance % (60));
 
                     // Display the result in the element with id="timer"
                     document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
@@ -254,9 +254,9 @@ function ajax_call() {
                     const finish_time = stage_time - (game_end - game_start);
                     console.log(finish_time);
                     console.log("game ended");
-                    const minutes = Math.floor((finish_time) / (60));
-                    const seconds = Math.floor(finish_time % (60));
-                    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+                    var end_minutes = Math.floor((finish_time) / (60));
+                    var end_seconds = Math.floor(finish_time % (60));
+                    document.getElementById("timer").innerHTML = end_minutes + "m " + end_seconds + "s ";
                     document.getElementById("timer").classList.add("complete");
                 } else if (game_state[0] == 0){
                     document.getElementById("timer").classList.remove("complete");
