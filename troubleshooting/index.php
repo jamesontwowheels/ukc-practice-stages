6d5534fd662f20aa05e0833b8860959db60e5446
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-10-16 09:41:43 +0000

    troubleshooting

---
+++ troubleshooting/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="/main.css">
+<link rel="manifest" href="/manifest.json">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Troubleshooting</title>
+</head>
+<body>
+    
+  <h2>MINDGAMES</h2>
+  <div id="main">
+    <h3>Troubleshooting</h3>
+    <ul>
+        <li><b>Location permissions</b> must be granted for mindgames to work. On android this is enabled through the browser, on iOS this is enabled through Safari and/or through settings</li>
+    </ul>
+    </div>
+</body>
+</html>
