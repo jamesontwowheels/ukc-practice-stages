ccdf9dc3bedd08673b982a80f2346f7a873ef344
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-11-20 21:46:25 +0000

    fix??

--- santa/assets/php/game.php
+++ santa/assets/php/game.php
@@ -428,6 +428,7 @@ if($debug == 1){ $debug_log[] = '72';};
     //ONCE WE HAVE CYCLED THROUGH THE CPs..
 
     $final_score = $running_score - $time_penalty;
+    $time = $game_time;
        //live results
        $live_result[$name]=$final_score;
    $results_summary[$id][] = [$name,$surname,$time,$running_score,-$time_penalty,$final_score,$id];
