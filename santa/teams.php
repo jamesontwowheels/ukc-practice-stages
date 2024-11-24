9e0181b242d0d0c9288f0c50ab7d3b8652902955
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-11-24 21:28:07 +0000

    ditch those

--- santa/teams.php
+++ santa/teams.php
@@ -81,8 +81,6 @@ $teams = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../main.css">  
     <link rel="stylesheet" href="assets/css/app-buttons.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
