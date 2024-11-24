b666aa555c9358e4f35ebcb0cf7e1d5d7a9ba724
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-11-24 21:19:54 +0000

    bump if logged out

--- santa/teams.php
+++ santa/teams.php
@@ -4,6 +4,14 @@ include 'assets/php/db_connect.php';
 
 // Start session for player identification
 session_start();
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    // Redirect to login page if not logged in
+    header("Location: ../index.php");
+    exit;
+}
+
 $player_id = $_SESSION['user_ID']; // Assuming player ID is stored in session
 $game = 4;
 $location = 0;
