50bcc292fb5f3d1ea2e96488b27cc34096d3de58
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2024-12-02 08:24:14 +0000

    try this one

--- thingdoer/assets/php/post.php
+++ thingdoer/assets/php/post.php
@@ -15,11 +15,10 @@ $createdAt = (new DateTime())->format('Y-m-d H:i:s'); // Format date as 'YYYY-MM
 //define the update
 $sql = "INSERT INTO dbo.thingdoer (Purpose, CreatedAt, InputValue, UserName) VALUES 
     ($purpose, $createdAt, $inputValue, $username);";
-$db_response[] = $sql;
+  
+echo $sql; 
 if ($conn->query($sql) == TRUE) {
     echo "record inserted successfully";
-    $last_id = $conn->insert_id;
-  echo "The success inserted ID is: " . $last_id . "<br>";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
     echo "The last inserted ID is: " . $last_id . "<br>";
