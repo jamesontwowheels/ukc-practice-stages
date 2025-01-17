0632329a5e35f79f0a777e89d87e9cef1baf3ced
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2025-01-17 15:45:46 +0000

    re-style

--- tribe/assets/js/test.js
+++ tribe/assets/js/test.js
@@ -191,9 +191,6 @@ function ajax_call() {
             
             // Optionally, set inner text or styles
             newDiv.innerHTML = `<div class="cp-header">`+ cp_names[this_key] +`</div><span class="close-btn">&times;</span>Option ` + this_key + '<button class="submit_button active" cp="' + this_key + '">Submit</button>';
-            newDiv.style.padding = '10px';
-            newDiv.style.border = '1px solid #ccc';
-            newDiv.style.margin = '5px 0';
             newDiv.classList.add('cp-option');
             
             // Append the new DIV to the parent DIV
