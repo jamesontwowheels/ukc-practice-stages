4e4981d0112f9d831e2eca0b01349209ba689538
jamesontwowheels <43553637+jamesontwowheels@users.noreply.github.com>
2025-04-04 12:17:38 +0000

    push now

--- snakes/assets/php/script.php
+++ snakes/assets/php/script.php
@@ -24,10 +24,11 @@ $input_time = time();
 $sql = "INSERT INTO dbo.test_game (Player_ID, CP_ID, Time_stamp, location, game, puzzle_answer, cp_option) VALUES 
     ($user_ID, $cp, $input_time, $location, $game, '$safe_input', $cp_option_choice);";
 $db_response[] = $sql;
-if ($conn->query($sql) == TRUE) {
+if (true)//($conn->query($sql) == TRUE) 
+    {
     $db_response[] =  "record inserted successfully";
-    $last_id = $conn->insert_id;
-    $db_response[] = "The success inserted ID is: " . $last_id . "<br>";
+    // $last_id = $conn->insert_id;
+    // $db_response[] = "The success inserted ID is: " . $last_id . "<br>";
 } else {
     $db_response[] = "Error: " . $sql . "<br>" . $conn->error;
     $db_response[] = "The last inserted ID is: " . $last_id . "<br>";
